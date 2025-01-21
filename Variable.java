8b1243a1c46dc44cca8fae8f8b5a8c9ca0c21733
Yash Tyagi <92726355+yashkt2525@users.noreply.github.com>
2025-01-21 21:10:51 +0530

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,15 @@
+public class Variable {
+    public static void main(String[] args) {
+        int a = 10;
+        long l = 8237434544L;
+        float f = 90.67f;
+        double d = 8937893893.353;
+        short s = 34;
+        byte b = 127;
+        char c = 97;
+        int ic = 'a';
+        String str = "Yash ";
+        System.out.println(Integer.MAX_VALUE);
+    }
+
+}
