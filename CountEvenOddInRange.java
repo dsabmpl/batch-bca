3d026371c5f9818b7870e07c727d7a44689f5f72
dsabmpl <dsabmpl@gmail.com>
2025-02-14 12:08:19 +0530

    Add files via upload

--- CountEvenOddInRange.java
+++ CountEvenOddInRange.java
@@ -1,7 +1,16 @@
 public class CountEvenOddInRange {
     static void countEvenOdd(int range,
             int evenCount, int oddCount) {
-
+        if (range == 0) {
+            System.out.println("Even " + evenCount + " Odd " + oddCount);
+            return;
+        }
+        if (range % 2 == 0) {
+            evenCount++;
+        } else {
+            oddCount++;
+        }
+        countEvenOdd(range - 1, evenCount, oddCount);
     }
 
     public static void main(String[] args) {
